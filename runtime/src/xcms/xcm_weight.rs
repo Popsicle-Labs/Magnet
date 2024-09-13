e57bad5d1d9186b054f59a1816c5bc8e22130947
sulijia <984115358@qq.com>
2024-09-13 13:22:05 +0800

    fix cargo fmt bug

--- runtime/src/xcms/xcm_weight.rs
+++ runtime/src/xcms/xcm_weight.rs
@@ -285,7 +285,8 @@ impl<
 		AccountId,
 		Currency: CurrencyT<AccountId>,
 		OnUnbalanced: OnUnbalancedT<Currency::NegativeImbalance>,
-	> WeightTrader for UsingComponentsEx<WeightToFee, AssetIdValue, AccountId, Currency, OnUnbalanced>
+	> WeightTrader
+	for UsingComponentsEx<WeightToFee, AssetIdValue, AccountId, Currency, OnUnbalanced>
 {
 	fn new() -> Self {
 		Self(Weight::zero(), Zero::zero(), PhantomData)
